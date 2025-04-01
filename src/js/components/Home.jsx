890322bd27751675bb6f8eacc25c7c25b587b35a
OzzieWP <ozziewpcontact@gmail.com>
2025-04-01 00:52:10 +0000

    Project done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,43 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
+import Footer from "./Footer";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+let cards = [
+	{title:"Gestor de Tareas Inteligente", description:"Optimiza tu tiempo como nunca antes. Esta aplicación te permite organizar tus actividades diarias de forma dinámica, priorizando las tareas más importantes mediante algoritmos de inteligencia artificial. Mantente enfocado en lo que realmente importa."},
+	{title:"Explorador de Recetas Globales", description:"Descubre los sabores del mundo desde tu pantalla. Encuentra recetas internacionales únicas según tus ingredientes disponibles y preferencias. Una herramienta para convertir tu cocina en un rincón del mundo."},
+	{title:"Analizador de Datos Deportivo", description:"Transforma estadísticas en estrategias ganadoras. Diseñado para entrenadores y fanáticos, este programa analiza estadísticas deportivas en tiempo real, ofreciendo insights clave para mejorar el rendimiento del equipo."},
+	{title:"Asistente de Finanzas Personales", description:"Toma el control de tus finanzas con facilidad. Planifica tus gastos, establece metas de ahorro y visualiza tu progreso con gráficos interactivos. Una herramienta esencial para alcanzar la estabilidad económica."},
+
+]
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-            
+            <Navbar/>
+			<Jumbotron/>
+		<div className="container">
+			<div className="row">
+				
+				{
+					cards.map((card,index) =>{
+						return <Card title={card.title} description={card.description}/>
+					})
+					
+				}
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
-	);
-};
+		<Footer/>
+		</div>
+	)
+}	
+
+	
+
 
 export default Home;
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,14 @@
+const Jumbotron = () => {
+  return (
+    <div className="container mt-5">
+      <div className="jumbotron bg-light p-5 rounded">
+        <h1 className="display-4">"Innovación a tu alcance"</h1>
+        <p className="lead">Explora un mundo de soluciones inteligentes diseñadas para transformar tus ideas en realidad. Desde la organización personal hasta la gestión de finanzas, nuestras herramientas combinan tecnología de vanguardia con creatividad para simplificar tu día a día y maximizar tu potencial.</p>
+        <hr className="my-4" />
+        <a className="btn btn-primary btn-lg" href="#" role="button">Aprende mas...</a>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
